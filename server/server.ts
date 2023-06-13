fa0fd6b4d17e3cd9d0a7a377cc2ba5d48892d3c9
Taro Schenker <taroschenker@hotmail.co.uk>
2023-06-13 15:40:14 +0100

    added mongo mongoose & connects to server

--- server/server.ts
+++ server/server.ts
@@ -1,7 +1,12 @@
-import express from 'express';
+import express from "express";
+import connectDb from "./src/config/db";
+import { config as dotenvConfig } from "dotenv";
+dotenvConfig();
+
 const app = express();
 const port = 3000;
 
+connectDb();
 app.listen(port, () => {
-    console.log(`Server running at http://localhost:${port}`);
+  console.log(`Server running at http://localhost:${port}`);
 });
