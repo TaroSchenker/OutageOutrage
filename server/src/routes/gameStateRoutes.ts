b076dc4925d4a5e9850a8ad24c2a20f29fabefbf
Taro Schenker <taroschenker@hotmail.co.uk>
2023-06-20 13:02:03 +0100

    added server readme

--- server/src/routes/gameStateRoutes.ts
+++ server/src/routes/gameStateRoutes.ts
@@ -6,7 +6,7 @@ const router = express.Router();
 
 router.post('/', gameStateController.initializeGame);
 router.post('/:gameId/turn', gameStateController.newTurn);
-// router.put('/tasks', gameStateController.processTaskAssignments);
+router.put('/tasks', gameStateController.processTaskAssignments);
 // router.put('/staff', gameStateController.reduceStaff);
 // router.put('/event', gameStateController.handleEvent);
 
