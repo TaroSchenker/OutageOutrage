ead695fb6bbc00b04536fd374e72a600e2b9a127
Taro Schenker <taroschenker@hotmail.co.uk>
2023-06-22 11:31:46 +0100

    client setup

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,34 +1,34 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import './App.css'
+import { useState } from 'react';
+import reactLogo from './assets/react.svg';
+import './App.css';
 
+/* write me some tailwindcss */
 function App() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(0);
 
   return (
     <div className="App">
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src="/vite.svg" className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://reactjs.org" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+      // tailwind css for a card
+      <div className="max-w-sm rounded overflow-hidden shadow-lg">
+        <img className="w-full" src={reactLogo} alt="React Logo" />
+        <div className="px-6 py-4">
+          <div className="font-bold text-xl mb-2">React</div>
+          <p className="text-gray-700 text-base">
+            A JavaScript library for building user interfaces
+          </p>
+
+          <div className="flex items-center justify-between mt-4">
+            <button
+              className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
+              onClick={() => setCount(count + 1)}
+            >
+              Click Me
+            </button>
+          </div>
+        </div>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
