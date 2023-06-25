bebfe014b1416f747791a662454ac8710c77c53c
Taro Schenker <taroschenker@hotmail.co.uk>
2023-06-25 10:59:34 +0100

    added error handling to fetchers

--- client/src/api/index.ts
+++ client/src/api/index.ts
@@ -4,135 +4,203 @@ const instance = axios.create({
   baseURL: '/api',
 });
 
-// Game requests
+async function handleErrors<T>(
+  promise: Promise<T>,
+): Promise<{ error: any; data?: T }> {
+  try {
+    const data = await promise;
+    return { error: null, data };
+  } catch (error) {
+    console.error('Error occurred:', error);
+    return { error };
+  }
+}
+
 export const getAllGames = async () => {
-  const { data } = await instance.get('/game');
+  const { error, data } = await handleErrors(instance.get('/game'));
+  if (error) {
+    throw error;
+  }
   return data;
 };
-
 export const getGameById = async (gameId: string) => {
-  const { data } = await instance.get(`/game/${gameId}`);
+  const { error, data } = await handleErrors(instance.get(`/game/${gameId}`));
+
+  if (error) {
+    throw error;
+  }
+
   return data;
 };
 
 export const createGame = async (game: object) => {
-  const { data } = await instance.post('/game', game);
+  const { error, data } = await handleErrors(instance.post('/game', game));
+
+  if (error) {
+    throw error;
+  }
+
   return data;
 };
 
 export const updateGame = async (gameId: string, updates: object) => {
-  const { data } = await instance.put(`/game/${gameId}`, updates);
+  const { error, data } = await handleErrors(
+    instance.put(`/game/${gameId}`, updates),
+  );
+
+  if (error) {
+    throw error;
+  }
+
   return data;
 };
 
 export const deleteGame = async (gameId: string) => {
-  const { data } = await instance.delete(`/game/${gameId}`);
+  const { error, data } = await handleErrors(
+    instance.delete(`/game/${gameId}`),
+  );
+
+  if (error) {
+    throw error;
+  }
+
   return data;
 };
 
-
 // GameState requests
 export const getGameState = async (gameId: string) => {
-    const { data } = await instance.get(`/gameState/${gameId}`);
-    return data;
-  };
-  
-  export const initializeGameState = async () => {
-    const { data } = await instance.post(`/gameState`);
-    return data;
-  };
-  
-  export const startNewTurn = async (gameId: string) => {
-    const { data } = await instance.post(`/gameState/${gameId}/turn`);
-    return data;
-  };
-  
+  const { error, data } = await handleErrors(
+    instance.get(`/gameState/${gameId}`),
+  );
+
+  if (error) {
+    throw error;
+  }
+
+  return data;
+};
+
+export const initializeGameState = async () => {
+  const { error, data } = await handleErrors(instance.post(`/gameState`));
+
+  if (error) {
+    throw error;
+  }
+
+  return data;
+};
+
+export const startNewTurn = async (gameId: string) => {
+  const { error, data } = await handleErrors(
+    instance.post(`/gameState/${gameId}/turn`),
+  );
+
+  if (error) {
+    throw error;
+  }
+
+  return data;
+};
 
 // Staff requests
 export const getAllStaff = async () => {
-  const { data } = await instance.get('/staff');
+  const { error, data } = await handleErrors(instance.get('/staff'));
+
+  if (error) {
+    throw error;
+  }
+
   return data;
 };
 
 export const getStaffById = async (staffId: string) => {
-  const { data } = await instance.get(`/staff/${staffId}`);
+  const { error, data } = await handleErrors(instance.get(`/staff/${staffId}`));
+
+  if (error) {
+    throw error;
+  }
+
   return data;
 };
+
 // Task requests
 export const getAllTasks = async () => {
-  const { data } = await instance.get('/tasks');
+  const { error, data } = await handleErrors(instance.get('/tasks'));
+
+  if (error) {
+    throw error;
+  }
+
   return data;
 };
-
-export const createStaff = async (staff: object) => {
-  const { data } = await instance.post('/staff', staff);
-  return data;
-};
-
-export const updateStaff = async (staffId: string, updates: object) => {
-  const { data } = await instance.put(`/staff/${staffId}`, updates);
-  return data;
-};
-
-export const deleteStaff = async (staffId: string) => {
-  const { data } = await instance.delete(`/staff/${staffId}`);
-  return data;
-};
-
-export const assignTask = async (staffId: string, taskId: string) => {
-  const { data } = await instance.put(`/staff/${staffId}/assignTask`, { taskId });
-  return data;
-};
-
-export const updateMorale = async (staffId: string, morale: number) => {
-  const { data } = await instance.put(`/staff/${staffId}/morale`, { morale });
-  return data;
-};
-
-
-export const createTask = async (task: string) => {
-  const { data } = await instance.post('/tasks', task);
-  return data;
-};
-
-export const updateTask = async (taskId: string, updates: string) => {
-  const { data } = await instance.put(`/tasks/${taskId}`, updates);
-  return data;
-};
-
-export const deleteTask = async (taskId: string) => {
-  const { data } = await instance.delete(`/tasks/${taskId}`);
-  return data;
-};
-
 // GameEvent requests
 export const getAllGameEvents = async () => {
-  const { data } = await instance.get('/gameEvents');
+  const { error, data } = await handleErrors(instance.get('/gameEvents'));
+
+  if (error) {
+    throw error;
+  }
+
   return data;
 };
 
 export const getGameEventById = async (gameEventId: string) => {
-  const { data } = await instance.get(`/gameEvents/${gameEventId}`);
+  const { error, data } = await handleErrors(
+    instance.get(`/gameEvents/${gameEventId}`),
+  );
+
+  if (error) {
+    throw error;
+  }
+
   return data;
 };
 
 export const createGameEvent = async (event: object) => {
-  const { data } = await instance.post('/gameEvents/events', event);
+  const { error, data } = await handleErrors(
+    instance.post('/gameEvents/events', event),
+  );
+
+  if (error) {
+    throw error;
+  }
+
   return data;
 };
 
 export const updateGameEvent = async (eventId: string, updates: object) => {
-  const { data } = await instance.put(`/gameEvents/events/${eventId}`, updates);
+  const { error, data } = await handleErrors(
+    instance.put(`/gameEvents/events/${eventId}`, updates),
+  );
+
+  if (error) {
+    throw error;
+  }
+
   return data;
 };
 
 export const deleteGameEvent = async (eventId: string) => {
-  const { data } = await instance.delete(`/gameEvents/events/${eventId}`);
+  const { error, data } = await handleErrors(
+    instance.delete(`/gameEvents/events/${eventId}`),
+  );
+
+  if (error) {
+    throw error;
+  }
+
   return data;
 };
 
 // GameState requests
 export const getGameStateById = async (gameStateId: string) => {
-  const { data } = await instance.get(`/gameState/${gameStateId}`);
+  const { error, data } = await handleErrors(
+    instance.get(`/gameState/${gameStateId}`),
+  );
+
+  if (error) {
+    throw error;
+  }
+
   return data;
 };
