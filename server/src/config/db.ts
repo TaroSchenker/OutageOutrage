e4b969c370637e43ea31ce6382440dcc429a2220
Taro Schenker <taroschenker@hotmail.co.uk>
2023-07-31 09:50:38 +0100

    chagned db back to mongo atlas online

--- server/src/config/db.ts
+++ server/src/config/db.ts
@@ -1,27 +1,10 @@
 //SETTINGS FOR MONGO ATLAS
-// import mongoose from 'mongoose';
-
-// const connectDB = async (): Promise<void> => {
-//   try {
-//     const conn = await mongoose.connect(process.env.MONGO_URI || '');
-//     console.log(`MongoDB connected: ${conn.connection.host}`);
-//   } catch (error) {
-//     console.error(error);
-//     process.exit(1);
-//   }
-// };
-
-// export default connectDB;
-
-//SETTINGS FOR MONGO LOCAL
 import mongoose from 'mongoose';
 
 const connectDB = async (): Promise<void> => {
   try {
-    const conn = await mongoose.connect(
-      'mongodb://localhost:27017/outageOutrage',
-    );
-    console.log(`MongoDB local connected: ${conn.connection.host}`);
+    const conn = await mongoose.connect(process.env.MONGO_URI || '');
+    console.log(`MongoDB connected: ${conn.connection.host}`);
   } catch (error) {
     console.error(error);
     process.exit(1);
@@ -29,3 +12,20 @@ const connectDB = async (): Promise<void> => {
 };
 
 export default connectDB;
+
+//SETTINGS FOR MONGO LOCAL
+// import mongoose from 'mongoose';
+
+// const connectDB = async (): Promise<void> => {
+//   try {
+//     const conn = await mongoose.connect(
+//       'mongodb://localhost:27017/outageOutrage',
+//     );
+//     console.log(`MongoDB local connected: ${conn.connection.host}`);
+//   } catch (error) {
+//     console.error(error);
+//     process.exit(1);
+//   }
+// };
+
+// export default connectDB;
