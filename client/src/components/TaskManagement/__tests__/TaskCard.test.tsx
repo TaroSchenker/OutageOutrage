0238ed61ffc01c5c783c57ecf88d5d2e831cdb48
Taro Schenker <taroschenker@hotmail.co.uk>
2023-08-08 10:22:13 +0100

    save

--- client/src/components/TaskManagement/__tests__/TaskCard.test.tsx
+++ client/src/components/TaskManagement/__tests__/TaskCard.test.tsx
@@ -1,8 +1,8 @@
 import React from 'react';
 import { render, screen } from '@testing-library/react';
 import userEvent from '@testing-library/user-event';
-import '@testing-library/jest-dom/extend-expect';
-
+// import '@testing-library/jest-dom/extend-expect';
+import '@testing-library/jest-dom';
 import { server } from '../../../mocks/node';
 import TaskCard from '../TaskCard';
 import {
