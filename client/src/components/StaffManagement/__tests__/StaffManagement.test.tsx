e10c6376a33232fa589626d6ef273ad2459b6dec
Taro Schenker <taroschenker@hotmail.co.uk>
2023-08-09 10:45:05 +0100

    added describe test block for a few comps

--- client/src/components/StaffManagement/__tests__/StaffManagement.test.tsx
+++ client/src/components/StaffManagement/__tests__/StaffManagement.test.tsx
@@ -25,4 +25,33 @@ describe('<StaffManagement />', () => {
             expect(screen.getByText(staff.name)).toBeInTheDocument();
         });
     });
+      // Check if the component renders without crashing
+  it('should render without crashing', () => {});
+
+  // Check if the correct headings are rendered for Available Staff and Busy Staff
+  it('should render the correct headings for Available and Busy Staff', () => {});
+
+  // Check if the component correctly separates staff into Available and Busy based on their availability
+  it('should correctly separate staff into Available and Busy categories', () => {});
+
+  // Check if StaffCard components are rendered for all available staff
+  it('should render StaffCard for all available staff', () => {});
+
+  // Check if StaffCard components are rendered for all not available staff
+  it('should render StaffCard for all busy staff', () => {});
+
+  // Check if the grid layout is correctly applied
+  it('should display the staff in the correct grid layout', () => {});
+
+  // Check if the correct number of StaffCard components are rendered
+  it('should render the correct number of StaffCard components based on the staff data', () => {});
+
+  // Check if the correct tasks are passed to each StaffCard
+  it('should pass the correct tasks to each StaffCard', () => {});
+
+  // Check if the component handles an empty staff array correctly (Optional)
+  it('should handle an empty staff array without errors', () => {});
+
+  // Check if the component handles edge cases, such as incorrect or missing props (Optional)
+  it('should handle edge cases gracefully', () => {});
 });
