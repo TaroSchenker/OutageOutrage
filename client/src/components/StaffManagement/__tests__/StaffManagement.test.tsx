e222c058f80961aa1e419ff49c8a275cc049d8c5
Taro Schenker <taroschenker@hotmail.co.uk>
2023-08-09 11:33:43 +0100

    added grid tests

--- client/src/components/StaffManagement/__tests__/StaffManagement.test.tsx
+++ client/src/components/StaffManagement/__tests__/StaffManagement.test.tsx
@@ -23,32 +23,45 @@ describe('<StaffManagement />', () => {
     });
   });
 
-    // Check if the grid layout is correctly applied
-    it('should display the staff in the correct grid layout', () => {
-        render(
-          <QueryClientProvider client={queryClient}>
-            <StaffManagement tasks={testTaskData} staff={testStaffData} />
-          </QueryClientProvider>,
-        );
-      
-        // Find the available and busy staff container divs
-        const availableStaffContainer = screen.getByText('Available Staff').nextSibling as HTMLElement;
-        const busyStaffContainer = screen.getByText('Busy Staff').nextSibling as HTMLElement;
-      
-        // Check if the containers are div elements and have the correct class
-        if (availableStaffContainer && availableStaffContainer.tagName === 'DIV') {
-          expect(availableStaffContainer.className).toContain('grid grid-cols-2 gap-4');
-        } else {
-          throw new Error('Available staff container not found or incorrect element type');
-        }
-      
-        if (busyStaffContainer && busyStaffContainer.tagName === 'DIV') {
-          expect(busyStaffContainer.className).toContain('grid grid-cols-2 gap-4');
-        } else {
-          throw new Error('Busy staff container not found or incorrect element type');
-        }
-      });
-    // Check if the correct tasks are passed to each StaffCard
-    it('should pass the correct tasks to each StaffCard', () => {});
+  // Check if the grid layout is correctly applied
+  it('should display the staff in the correct grid layout', () => {
+    render(
+      <QueryClientProvider client={queryClient}>
+        <StaffManagement tasks={testTaskData} staff={testStaffData} />
+      </QueryClientProvider>,
+    );
 
+    // Find the available and busy staff container divs
+    const availableStaffContainer = screen.getByText('Available Staff')
+      .nextSibling as HTMLElement;
+    const busyStaffContainer = screen.getByText('Busy Staff')
+      .nextSibling as HTMLElement;
+
+    // Check if the containers are div elements and have the correct class
+    if (availableStaffContainer && availableStaffContainer.tagName === 'DIV') {
+      expect(availableStaffContainer.className).toContain(
+        'grid grid-cols-2 gap-4',
+      );
+    } else {
+      throw new Error(
+        'Available staff container not found or incorrect element type',
+      );
+    }
+
+    if (busyStaffContainer && busyStaffContainer.tagName === 'DIV') {
+      expect(busyStaffContainer.className).toContain('grid grid-cols-2 gap-4');
+    } else {
+      throw new Error(
+        'Busy staff container not found or incorrect element type',
+      );
+    }
   });
+  // Check if the correct tasks are passed to each StaffCard
+  it('should pass the correct tasks to each StaffCard', () => {
+    render(
+      <QueryClientProvider client={queryClient}>
+        <StaffManagement tasks={testTaskData} staff={testStaffData} />
+      </QueryClientProvider>,
+    );
+  });
+});
