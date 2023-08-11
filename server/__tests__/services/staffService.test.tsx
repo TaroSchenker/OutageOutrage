d5590e4f02880bf051f0c8dc876a979ef7ee4d9c
George Barrett <georgeaustinbarrett@gmail.com>
2023-08-11 10:56:03 +0100

    beforeEach added to staffService test suite. staffData and staffMembers variables made global

--- server/__tests__/services/staffService.test.tsx
+++ server/__tests__/services/staffService.test.tsx
@@ -2,7 +2,60 @@ import mongoose from 'mongoose';
 import { MongoMemoryServer } from 'mongodb-memory-server';
 import { StaffService } from '../../src/services/staffService';
 
-import { Expertise, IStaffData, Role } from '../../src/types/types';
+import { Expertise, IStaff, IStaffData, Role } from '../../src/types/types';
+import { getAllStaff } from '../../src/controllers/staffController';
+
+const staffData: IStaffData = {
+  name: 'Test',
+  avatarUrl: 'https://test.com',
+  role: Role.BACKEND_DEV,
+  expertise: Expertise.JAVA,
+  ambition: 80,
+  loyalty: 80,
+  skillLevel: 80,
+  resilience: 80,
+  adaptability: 80,
+  morale: 80,
+  currentTask: null,
+  availability: true,
+  salary: 50000,
+  satisfaction: 100,
+};
+
+const staffMembers: IStaffData[] = [
+  {
+    name: 'Test',
+    avatarUrl: 'https://test.com',
+    role: Role.BACKEND_DEV,
+    expertise: Expertise.JAVA,
+    ambition: 80,
+    loyalty: 80,
+    skillLevel: 80,
+    resilience: 80,
+    adaptability: 80,
+    morale: 80,
+    currentTask: null,
+    availability: true,
+    salary: 50000,
+    satisfaction: 100,
+  },
+  {
+    name: 'Test2',
+    avatarUrl: 'https://test2.com',
+    role: Role.FRONTEND_DEV,
+    expertise: Expertise.JAVASCRIPT,
+    ambition: 70,
+    loyalty: 70,
+    skillLevel: 70,
+    resilience: 70,
+    adaptability: 70,
+    morale: 70,
+    currentTask: null,
+    availability: true,
+    salary: 90000,
+    satisfaction: 90,
+  },
+];
 
 describe('StaffService', () => {
   // let con: MongoClient;
@@ -22,30 +75,37 @@ describe('StaffService', () => {
     await mongoServer.stop();
   });
 
-  it('should create a new staff member', async () => {
-    const staffData: IStaffData = {
-      name: 'Test',
-      avatarUrl: 'https://test.com',
-      role: Role.BACKEND_DEV,
-      expertise: Expertise.JAVA,
-      ambition: 80,
-      loyalty: 80,
-      skillLevel: 80,
-      resilience: 80,
-      adaptability: 80,
-      morale: 80,
-      currentTask: null,
-      availability: true,
-      salary: 50000,
-      satisfaction: 100,
-    };
+  beforeEach(async () => {
+    staffMembers.map(async (staff) => {
+      void (await staffService.createStaff(staff));
+    });
+  });
 
+  // afterEach();
+
+  it('should create a new staff member', async () => {
     const staff = await staffService.createStaff(staffData);
     expect(staff.name).toBe(staffData.name);
     expect(staff.avatarUrl).toBe(staffData.avatarUrl);
     expect(staff.role).toBe(staffData.role);
     expect(staff.expertise).toBe(staffData.expertise);
+    expect(staff.ambition).toBe(staffData.ambition);
+    expect(staff.loyalty).toBe(staffData.loyalty);
+    expect(staff.skillLevel).toBe(staffData.skillLevel);
+    expect(staff.resilience).toBe(staffData.resilience);
+    expect(staff.adaptability).toBe(staffData.adaptability);
+    expect(staff.morale).toBe(staffData.morale);
+    expect(staff.currentTask).toBe(staffData.availability);
+    expect(staff.salary).toBe(staffData.satisfaction);
   });
 
-  // TODO: Other tests for get, update, delete operations
+  it('should get all the staff', async () => {
+    void (await staffService.createStaff(staffMembers[0]));
+    void (await staffService.createStaff(staffMembers[1]));
+    const allStaff = await staffService.getAllStaff();
+
+    console.log(allStaff);
+
+    expect(allStaff.length).toBe(staffMembers.length);
+  });
 });
