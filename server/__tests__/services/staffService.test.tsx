e9c9e77bc8954ab71c15f655e72f09311a870a8a
Taro Schenker <taroschenker@hotmail.co.uk>
2023-08-21 11:59:56 +0100

    staffService first test

--- server/__tests__/services/staffService.test.tsx
+++ server/__tests__/services/staffService.test.tsx
@@ -1,7 +1,7 @@
 import mongoose from 'mongoose';
 import { MongoMemoryServer } from 'mongodb-memory-server';
 import { StaffService } from '../../src/services/staffService';
-
+import { staff as staffTestData } from '../../src/config/initialData/staff';
 import { Expertise, IStaffData, Role } from '../../src/types/types';
 
 describe('StaffService', () => {
@@ -23,28 +23,54 @@ describe('StaffService', () => {
   });
 
   it('should create a new staff member', async () => {
-    const staffData: IStaffData = {
-      name: 'Test',
-      avatarUrl: 'https://test.com',
-      role: Role.BACKEND_DEV,
-      expertise: Expertise.JAVA,
-      ambition: 80,
-      loyalty: 80,
-      skillLevel: 80,
-      resilience: 80,
-      adaptability: 80,
-      morale: 80,
-      currentTask: null,
-      availability: true,
-      salary: 50000,
-      satisfaction: 100,
-    };
+    const staff = await staffService.createStaff(staffTestData[0]);
 
-    const staff = await staffService.createStaff(staffData);
-    expect(staff.name).toBe(staffData.name);
-    expect(staff.avatarUrl).toBe(staffData.avatarUrl);
-    expect(staff.role).toBe(staffData.role);
-    expect(staff.expertise).toBe(staffData.expertise);
+    for (const key in staff) {
+      if (staff.hasOwnProperty(key)) {
+        const typedKey = key as keyof IStaffData;
+        expect((staff as any)[key]).toBe(staff[typedKey]);
+      }
+    }
+  });
+
+  test('getAllStaff should retrieve all staff members', () => {
+    // TODO: Implement the test logic here
+  });
+
+  test('getStaffById should retrieve a staff member by ID', () => {
+    // TODO: Implement the test logic here
+  });
+
+  test('createStaff should create a new staff member', () => {
+    // TODO: Implement the test logic here
+  });
+
+  test('updateStaff should update a staff member', () => {
+    // TODO: Implement the test logic here
+  });
+
+  test('deleteStaff should delete a staff member', () => {
+    // TODO: Implement the test logic here
+  });
+
+  test('assignTask should assign a task to a staff member', () => {
+    // TODO: Implement the test logic here
+  });
+
+  test('removeTask should remove a task from a staff member', () => {
+    // TODO: Implement the test logic here
+  });
+
+  test('updateMorale should update staff morale', () => {
+    // TODO: Implement the test logic here
+  });
+
+  test('calculateTotalStaffCost should calculate the total cost of given staff members', () => {
+    // TODO: Implement the test logic here
+  });
+
+  test('calculateTotalStaffMorale should calculate the average morale of given staff members', () => {
+    // TODO: Implement the test logic here
   });
 
   // TODO: Other tests for get, update, delete operations
